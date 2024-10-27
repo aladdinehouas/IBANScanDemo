6cd096a32290498e88456b6efae23ca739318862
Aladdin HOUAS <aladdinhouas@gmail.com>
2024-10-27 15:27:48 +0100

    Add .gitignore to ignorer DerivedData

---
+++ IbanScanDemo/IbanScanDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IbanScanDemoApp.swift
+//  IbanScanDemo
+//
+//  Created by Aladdin Houas on 27/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct IbanScanDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
